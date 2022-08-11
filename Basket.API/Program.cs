16403fda6b2c521ec8fe0e4b860b458e2abfb935
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-08-11 19:27:35 -0300

    create basket repository

--- Basket.API/Program.cs
+++ Basket.API/Program.cs
@@ -1,3 +1,6 @@
+using Basket.API.Core.Interfaces;
+using Basket.API.Data.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 var app = builder.Build();
 
