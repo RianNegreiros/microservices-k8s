97255f655668fcb00e0b62f2920726b607e79202
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-08-13 15:01:03 -0300

    Discount gRPC initial commit

---
+++ Discount.Grpc/Program.cs
@@ -0,0 +1,19 @@
+using Discount.Grpc.Core.Interfaces;
+using Discount.Grpc.Extensions;
+using Discount.Grpc.Infra.Data.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+app.MigrateDatabase<Program>();
+
+// Configure the HTTP request pipeline.
+app.Run();
