13a632617e795f8d7e79f2529db7b167f4bd5d3f
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-08-14 18:09:28 -0300

    fix typo

--- Services/Ordering/Ordering.Domain/Entities/Order.cs
+++ Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -7,7 +7,7 @@ public class Order : EntityBase
     public string Username { get; set; }
     public decimal TotalPrice { get; set; }
     
-    public string FisrtName { get; set; }
+    public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
     public string Address { get; set; }
