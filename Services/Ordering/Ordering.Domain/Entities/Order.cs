a8fa7cc0c1e88c6af97fdf63a398c5495f7f79aa
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-08-15 20:32:16 -0300

    update entity to accept null

--- Services/Ordering/Ordering.Domain/Entities/Order.cs
+++ Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -15,9 +15,9 @@ public class Order : EntityBase
     public string State { get; set; }
     public string ZipCode { get; set; }
 
-    public string CardName { get; set; }
-    public string CardNumber { get; set; }
-    public string Expiration { get; set; }
-    public string CVV { get; set; }
-    public int PaymentMethod { get; set; }
+    public string? CardName { get; set; }
+    public string? CardNumber { get; set; }
+    public string? Expiration { get; set; }
+    public string? CVV { get; set; }
+    public int? PaymentMethod { get; set; }
 }
