3006874f7af0807972e3a0cbaf8daffdea461f15
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-08-15 20:35:46 -0300

    create registrations

--- Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
+++ Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
 using Ordering.Application.Exception;
 
 namespace Ordering.Application;
@@ -13,8 +14,9 @@ public static class ApplicationServiceRegistration
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddMediatR(Assembly.GetExecutingAssembly());
-        
+
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
 
         return services;
     }
