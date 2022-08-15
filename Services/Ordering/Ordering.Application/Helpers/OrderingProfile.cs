76c86c4c1edd86c9f1e725846bfd7174c95642a5
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-08-15 11:18:41 -0300

    create checkout command

---
+++ Services/Ordering/Ordering.Application/Helpers/OrderingProfile.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Helpers;
+
+public class OrderingProfile : Profile
+{
+    public OrderingProfile()
+    {
+        CreateMap<Order, OrdersVm>().ReverseMap();
+        CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+        CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+    }
+}
