57965c583295fcaa43656b9cb9ebbced1346b528
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-08-16 20:39:28 -0300

    create extensions for http client

---
+++ ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,16 @@
+using System.Text.Json;
+
+namespace Shopping.Aggregator.Extensions;
+
+public static class HttpClientExtensions
+{
+    public static async Task<T?> ReadContent<T>(this HttpResponseMessage response)
+    {
+        if (!response.IsSuccessStatusCode) throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
+
+        var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+        return JsonSerializer.Deserialize<T>(dataAsString,
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+    }
+}
