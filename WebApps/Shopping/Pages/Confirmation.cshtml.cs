9812927eb40ce5d553dbc8f54be7d5427dbc7327
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-08-23 23:37:50 -0300

    create confirmation and contact page

---
+++ WebApps/Shopping/Pages/Confirmation.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shopping.Pages
+{
+    public class ConfirmationModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGetContact()
+        {
+            Message = "Your email was sent.";
+        }
+
+        public void OnGetOrderSubmitted()
+        {
+            Message = "Your order submitted successfully.";
+        }
+    }
+}
---
+++ WebApps/Shopping/Pages/Contact.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shopping.Pages;
+
+public class Contact : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
